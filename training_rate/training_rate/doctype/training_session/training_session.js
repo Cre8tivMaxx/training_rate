27e8914eb26f66b0f23afe7d59f822ac8949193e
notabdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-01-18 16:19:13 +0200

    feat: filter trainer by logged in user

--- training_rate/training_rate/doctype/training_session/training_session.js
+++ training_rate/training_rate/doctype/training_session/training_session.js
@@ -5,4 +5,17 @@ frappe.ui.form.on("Training Session", {
 	refresh(frm) {
 		frm.add_web_link(`/training-feedback/new/?session=${frm.doc.name}`, "See on Website");
 	},
+	async setup(frm) {
+		const filterTrainer = await frappe.db.get_single_value(
+			"Training Rate Config",
+			"filter_trainer_by_current_user",
+		);
+		if (cint(filterTrainer) === 1) {
+			frm.set_query("trainer", () => ({
+				filters: {
+					user: frappe.session.user,
+				},
+			}));
+		}
+	},
 });
